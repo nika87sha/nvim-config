54fc7346940e2dbbb924b66d2b9feafec5012856
Veronica Duran <veronica.duran87@proton.me>
2025-08-19 09:41:22 +0200

    Se corrige la configuración de mason para la ultima version

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,45 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false, -- se carga al inicio
-        priority = 1000,
-        config = function()
-            require("mason").setup()
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp", -- opcional, para autocompletado
+  },
+  config = function()
+    -- Inicializa Mason
+    require("mason").setup()
+
+    -- Inicializa mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason_lspconfig.setup({
+      ensure_installed = { "lua_ls", "pyright", "tsserver" }, -- ajusta a tus LSP
+      automatic_installation = true, -- ✅ solo boolean o lista
+    })
+
+    -- Capabilities para autocompletado
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- Configuración de servidores LSP
+    local lspconfig = require("lspconfig")
+
+    -- Si existe setup_handlers (versiones nuevas)
+    if mason_lspconfig.setup_handlers then
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
         end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup()
-        end,
-    },
+      })
+    else
+      -- 🔙 fallback para versiones viejas
+      for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+        })
+      end
+    end
+  end,
 }
 
