54ceac7f9f7c950fc36a83984f2bd13f3c51e689
Verónica Durán <veronica.duran87@proton.me>
2026-01-28 17:35:45 +0100

    fix: change config lazygit on snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,9 +2,26 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
 
   dependencies = {
-    "kdheepak/lazygit.nvim",
     "nvim-tree/nvim-web-devicons",
   },
 
@@ -13,7 +30,7 @@ return {
     local icons = require("lib.icons")
 
     -- =========================
-    -- Layout helpers (FALTABAN)
+    -- Layout helpers 
     -- =========================
     local function files_layout(width)
       return {
@@ -144,6 +161,13 @@ return {
 
       explorer = { enabled = false },
     })
+
+       -- =========================
+    -- Keymaps (WHICH-KEY OK)
+    -- =========================
+    vim.keymap.set("n", "<leader>gg", function()
+      snacks.lazygit()
+    end, { desc = "Git: LazyGit" })
   end,
 }
 
